d27320116de621067b8bba56fe1504bba71a6a9c
Dan <dannguyen90@gmail.com>
2016-12-06 20:56:19 -0500

    add create and update actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,37 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     render 'show.json.jbuilder'
   end
+
+  def create
+    @movie = Movie.create(
+      title: params[:title],
+      duration: params[:duration],
+      genre: params[:genre],
+      description: params[:description]
+      ) 
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if params[:title]
+      @movie.title = params[:title]
+    end
+    if params[:duration]
+      @movie.title = params[:duration]
+    end
+    if params[:genre]
+      @movie.title = params[:genre]
+    end
+    if params[:description]
+      @movie.title = params[:description]
+    end
+    @movie.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/movies' => 'movies#index'
   get '/movies/:id' => 'movies#show'
+  post 'movies' => 'movies#create'
+  patch 'movies/:id' => 'movies#update'
+  delete 'movies/:id' => 'movies#destroy'
 end
